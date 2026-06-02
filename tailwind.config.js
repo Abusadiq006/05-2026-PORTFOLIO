c0a46e906c9ac29d7c8a2448a62ee1e5a9c2ef86
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-06-02 04:46:31 -0700

    theme:

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,6 @@ module.exports = {
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
-    ]
+    ],
+    theme: {}
 }
