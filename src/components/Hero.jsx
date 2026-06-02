55c5af30e116ee54eee0b3a51321a16196505236
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-06-02 06:15:46 -0700

    opacity: 1,

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ export default function Hero() {
     const itemVariants = {
         hidden: { opacity: 0, y: 30 },
         visible: {
-            
+            opacity: 1,
         }
     }
 }   
