1222c1766457398cd96e5778e87bdb00247884af
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-06-02 04:56:49 -0700

    updating layoout title

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
             animation: {
                 'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
             }
-        }
-    }
+        },
+    },
     plugins: [],
 }
