918a7f29ce4dd06827fc35310038667dda033c9f
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-06-02 04:45:56 -0700

    src styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,6 @@ module.exports = {
     content: [
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
     ]
 }
