22be55c12558992238213c16f41760bee3b7d324
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-06-02 06:59:21 -0700

    initiate section

--- src/components/WorkSection.jsx
+++ src/components/WorkSection.jsx
@@ -4,5 +4,7 @@ import { motion } from "framer-motion";
 import { PROJECTS } from "@/constants/portfolioData";
 
 export default function WorkSection() {
-    return ()
+    return (
+        <section className="min-h-screen bg-slate-950 text-slate-100 py-20 px-4 sm:px-8 lg:px-16 border-t border-slate-900"></section>
+    )
 }
