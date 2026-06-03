718082113ce9f2b111b7330e0c4942b9fb13bd71
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-06-03 05:39:51 -0700

    upadating tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
     ],
     theme: {
         extend: {
+            fontFamily: {
+                sans: ['var(--font-geist-sans)'],
+                mono: ['var(--font-geist-mono)'],
+            },
             animation: {
                 'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
             }
