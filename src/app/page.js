b84cc8d0cb5baa711cffd5d561edb913a24b9657
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-06-03 04:56:39 -0700

    return (

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,8 @@
 import Hero from "@/components/Hero"
 import WorkSection from "@/components/WorkSection"
 
-export default function Home() {}
+export default function Home() {
+  return(
+    
+  )
+}
