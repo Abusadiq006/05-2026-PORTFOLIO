29eedf87234b7ab84cc1d867d0395a1e9342b6ab
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-06-03 04:57:10 -0700

    main

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,10 @@ import WorkSection from "@/components/WorkSection"
 
 export default function Home() {
   return(
-    
+    <main className="flex-1">
+      <Hero />
+      <WorkSection />
+      {/* Future sections like Experience, Tech Stack Matrix, or Contact will plug in here */}
+    </main>
   )
 }
