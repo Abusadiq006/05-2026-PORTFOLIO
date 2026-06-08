4a8dc24facf5b336cb377dbaaad4dbbcde5d3fe3
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-06-08 06:03:49 -0700

    updating expertise into d server

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero"
 import WorkSection from "@/components/WorkSection"
+import Expertise from "@/components/Expertise"
 
 export default function Home() {
   return(
     <main className="flex-1">
       <Hero />
+      <Expertise />
       <WorkSection />
       {/* Future sections like Experience, Tech Stack Matrix, or Contact will plug in here */}
     </main>
