f7b6755ed18e2f4721d39a916357d4165953f977
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-06-10 07:06:41 -0700

    cont.

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Hero from "@/components/Hero"
 import WorkSection from "@/components/WorkSection"
 import Expertise from "@/components/Expertise"
 import Contact from "@/components/Contact"
+import contact from "@/app/actions/contact"
 
 export default function Home() {
   return(
     <main className="flex-1">
       <Hero />
       <Expertise />
+      <contact/>
       <Contact />
       <WorkSection />
       {/* Future sections like Experience, Tech Stack Matrix, or Contact will plug in here */}
--- src/constants/portfolioData.jsx
+++ src/constants/portfolioData.jsx
@@ -1,5 +1,5 @@
 export const HERO_DATA = {
-    accent: "01 // SYSTEM LOG INITIALIZED",
+    accent: "||FARMER||ABUSADIQ||PORTFOLIO||",
     mainTitle: "Engineering Clean Backend Logic & Dynamic Interfaces",
     subTitle: "Full-Stack Software Architect specializing in resilient distributed systems, secure payment architectures, and hyper-responsive user ecosystems.",
     metrics: [
